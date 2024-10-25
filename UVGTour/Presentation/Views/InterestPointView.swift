fc69db6580b227427c52bb85e6018793ba85f07a
Guillermo Santos <guillermo.santosba@gmail.com>
2024-10-24 18:32:28 -0600

    Added interest points.

---
+++ UVGTour/Presentation/Views/InterestPointView.swift
@@ -0,0 +1,18 @@
+//
+//  InterestPointView.swift
+//  UVGTour
+//
+//  Created by Guillermo Santos Barrios on 10/24/24.
+//
+
+import SwiftUI
+
+struct InterestPointView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InterestPointView()
+}
